0e98d7ffa9f8e58d54e01b38bd3434f4a73e33a8
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-07-15 16:48:38 +0800

    add 1934. Confirmation Rate

---
+++ 1934. Confirmation Rate/Solution.sql
@@ -0,0 +1,4 @@
+SELECT Signups.user_id, ROUND(AVG(IF(action = 'confirmed', 1, 0)),2) AS confirmation_rate
+FROM Signups LEFT JOIN Confirmations
+ON Signups.user_id = Confirmations.user_id
+GROUP BY user_id
