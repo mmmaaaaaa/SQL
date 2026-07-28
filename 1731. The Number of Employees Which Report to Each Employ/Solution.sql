5a2c128d9423e2ebdd54446c7f76584874040fe5
mmmaaaaaa <411035006@gms.ndhu.edu.tw>
2026-07-28 16:13:32 +0800

    add 1731. The Number of Employees Which Report to Each Employ

---
+++ 1731. The Number of Employees Which Report to Each Employ/Solution.sql
@@ -0,0 +1,5 @@
+SELECT e2.employee_id, e2.name, COUNT(e1.reports_to) AS reports_count, ROUND(AVG(e1.age)) AS average_age
+FROM Employees e1 JOIN Employees e2
+ON e1.reports_to = e2.employee_id
+GROUP BY e2.employee_id
+ORDER BY e2.employee_id
